d6f916161b0a26dd428459a18721afef500aaed7
Dasa <dasa.podkulova@gmail.com>
2024-01-24 20:00:09 +0100

    Prasatko

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -13,6 +13,67 @@ public class HlavniProgram {
     zofka = new Turtle();
 
     //TODO tady bude kód
+
+    /* ----> vytvor prasatko <----*/
+    prasatko();
+    nakresliNozky();
+    hlava();
+    ocasek();
+  }
+
+  public  void prasatko() {
+    zofka.turnLeft(90.0);
+    for (int i = 0; i < 2; i++) {
+      zofka.move(120.0);
+      zofka.turnRight(90.0);
+      zofka.move(60.0);
+      zofka.turnRight(90.0);
+    }
+  }
+
+  public void hlava() {
+    zofka.turnRight(30.0);
+    zofka.move(60);
+    zofka.turnRight(120);
+    zofka.move(60);
+  }
+
+
+  public void nakresliNozky() {
+    //zadní pár
+    zofka.turnLeft(45.0);
+    zofka.move(20.0);
+    zofka.turnRight(180.0);
+    zofka.move(20.0);
+    zofka.turnRight(90.0);
+    zofka.move(20.0);
+    zofka.turnRight(180.0);
+    zofka.move(20.0);
+    zofka.turnLeft(45.0);
+    //přední pár
+    zofka.move(120);
+
+    zofka.turnLeft(45.0);
+    zofka.move(20.0);
+    zofka.turnRight(180.0);
+    zofka.move(20.0);
+    zofka.turnRight(90.0);
+    zofka.move(20.0);
+    zofka.turnRight(180.0);
+    zofka.move(20.0);
+    zofka.turnLeft(45.0);
+  }
+  public void ocasek() {
+    zofka.penUp();
+    zofka.turnRight(30.0);
+    zofka.move(120);
+    zofka.turnRight(90);
+    zofka.move(30);
+    zofka.turnLeft(90);
+    zofka.penDown();
+    zofka.move(20);
+    zofka.penUp();
+    zofka.turnLeft(90);
   }
 
 }
